677b90020d92d28a4381855deb47d9f04c4e0285
Amin Zayeromali <amin.zayeromali@gmail.com>
2021-11-06 20:41:14 +0330

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,28 +1,23 @@
+# Import Python libs
 import pytse_client as tse
-from pytse_client import download_client_types_records
 import matplotlib.pyplot as plt
-from tkinter import *
 import arabic_reshaper
 from bidi.algorithm import get_display
 import pandas as pd
+import pandas_ta as ta
 
+
+# fetch one of Tricker Stock Price
 ticker = tse.download(symbols="نوری", write_to_csv=True, include_jdate=True)
 ticker = tse.Ticker("نوری")
 
+# Preprocessing Stock Persian Name (UTF-8)
 reshaped_text = arabic_reshaper.reshape('نمایش قیمت و حجم سهام با نماد نوری')
 Prtext = get_display(reshaped_text)
 
-history = ticker.history
-x1=history["date"]
-y1=history["close"]
-x2=x1
-y2=history["volume"]
+#Preprocessing & clean Data & Create Data Frame
+df=ticker.history[['jdate', 'volume' ,'close']]  # Fetch History of trades & clean data
+df.ta.ema(close='close',length=10,append=True)   # Add Technical feature EMA 
+df=df.iloc[10:]                                # Drop 10 NaN Values
 
-plt.plot(x1, y1, label = "line 1")
-#plt.plot(x2, y2, label = "line 2")
-#plt.bar(x2,y2)
-plt.xlabel('x - Days')
-plt.ylabel('y - Price & Volume')
-plt.title(Prtext)
-#plt.legend()
-plt.show()
+
